72e888159d0d2264d6216c0d1736f75a1e0a858c
parva shah <parvashah@parvas-MacBook-Pro.local>
2023-02-24 20:27:03 -0500

    FastAPI

--- Functions/auth_handler.py
+++ Functions/auth_handler.py
@@ -5,9 +5,10 @@ import jwt
 from decouple import config
 
 
-JWT_SECRET = config("secret")
-JWT_ALGORITHM = config("algorithm")
-
+# JWT_SECRET = config("secret")
+# JWT_ALGORITHM = config("algorithm")
+JWT_SECRET = "abcdefg"
+JWT_ALGORITHM = HS256
 
 def token_response(token: str):
     return {
